b15445ab09aa988ae564c5186e90e254c41efe3a
wilkes <wilkesjoiner@gmail.com>
2010-04-19 15:18:03 -0500

    reworked GET and POST macros

--- src/github_clj.clj
+++ src/github_clj.clj
@@ -2,7 +2,7 @@
   (:use [clojure.contrib.json.read :only [read-json-string]]
         [clojure.contrib.def :only [defvar]]
         [clojure.http.client :only [url-encode request]]
-        [clojure.contrib.str-utils :only [re-sub]]))
+        [clojure.contrib.str-utils :only [re-sub re-gsub]]))
 
 (def base-url "http://github.com/api/v2/json/")
 
@@ -32,6 +32,7 @@
     (reduce (fn [r [k v]] (conj r {(f k) (g v)}))
             {}
             m)))
+
 (defn stringify-keys [m]
   (apply-to-keys (fn [k] (if (keyword? k) (name k) (str k))) m))
 
@@ -41,12 +42,31 @@
 (defn sub-action [action]
   (re-sub #"\*login\*" *login* action))
 
+(defn parse-url-str [s]
+  [(re-gsub #":[a-z]+" "%s" s)
+   (vec (map (comp symbol second) (re-seq #":([a-z]+)" s)))])
+
 (defn parse-response [response]
   (let [[body] (:body-seq response)
         code (:code response)]
     (if (= 200 code)
       (keywordify-keys (read-json-string body))
       (throw (Exception. (str code " " (:msg response)))))))
+
+(defn make-key-mapper [f keys]
+  (fn [m]
+    (reduce (fn [result k]
+              (conj result (if-let [v (get m k)]
+                             {(f k) v}
+                             nil)))
+            {}
+            keys)))
+
+(defn nested [& keys]
+  (make-key-mapper #(str "values[" (name k) "]") keys))
+
+(defn flat [& keys]
+  (make-key-mapper identity keys))
 
 (defn do-get [action]
   (parse-response (request (str base-url action "?" (url-encode (auth-info))))))
@@ -60,64 +80,73 @@
                              (stringify-keys (merge params (auth-info)))
                              (str params "&" (url-encode (auth-info)))))))
 
-(defmacro GET [name action-base action-params]
-  `(defn ~name [~@action-params]
-     (let [escaped-params# (doall (map url-encode [~@action-params]))]
-       (do-get (apply format ~action-base escaped-params#)))))
+(defmacro GET [name url-pattern]
+  (let [[action-base action-params] (parse-url-str url-pattern)]
+      `(defn ~name [~@action-params]
+         (do-get (apply format ~action-base
+                        (doall (map url-encode ~action-params)))))))
 
-(defmacro POST [name action-base params]
-  `(defn ~name [~@params]
-     (let [action-params# [~(butlast params)]
-           form-params# ~(last params)
-           escaped-params# (doall (map url-encode action-params#))]
-       (do-post (apply format (sub-action ~action-base) escaped-params#)
-                form-params#))))
+(defmacro POST [name url-pattern & [keys-fn]]
+  (let [[action-base params] (parse-url-str url-pattern)
+        keys-fn (or keys-fn identity)]
+    `(defn ~name [~@params & [form-params#]]
+       (do-post (apply format (sub-action ~action-base)
+                       (doall (map url-encode ~params)))
+                (~keys-fn form-params#)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; User API
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(GET   search-for-users  "user/search/%s"          [s])
-(GET   get-user-info     "user/show/%s"            [username])
-(POST  update-user-info  "user/show/*login*"       [map-info])
-(GET   list-following    "user/show/%s/following"  [username])
-(GET   list-followers    "user/show/%s/followers"  [username])
-(GET   watched-repos     "repos/watched/%s"        [username])
-(GET   list-keys         "user/keys"               [])
-(POST  add-key           "user/key/add"            [key-map])
-(POST  remove-key        "user/key/remove"         [key-map])
-(GET   list-emails       "user/emails"             [])
+(GET   search-for-users  "user/search/:q")
+(GET   get-user-info     "user/show/:user")
+(POST  update-user-info  "user/show/*login*" (nested :name :email :blog
+                                                     :company :location))
+(GET   list-following    "user/show/:user/following")
+(GET   list-followers    "user/show/:user/followers")
+(GET   watched-repos     "repos/watched/:user")
+(GET   list-keys         "user/keys")
+(POST  add-key           "user/key/add"      (flat :title :key))
+(POST  remove-key        "user/key/remove"   (flat :id))
+(GET   list-emails       "user/emails")
 (comment
-  (POST add-email         "user/email/add"         [email-map])
-  (POST remove-email      "user/email/remove"      [email-map]))
+  (POST add-email         "user/email/add"    (flat :emails))
+  (POST remove-email      "user/email/remove" (flat :emails)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Repositories API
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(GET   search-repos         "repos/search/%s"                   [s])
-(GET   get-repo-info        "repos/show/%s/%s"                  [username repo-name])
-(POST  update-repo-info     "repos/show/%s/%s"                  [username repo-name map-info])
-(GET   list-all-repos       "repos/show/%s"                     [username])
-(POST  watch-repo           "repos/watch/%s/%s"                 [username repo-name])
-(POST  unwatch-repo         "repos/unwatch/%s/%s"               [username repo-name])
-(POST  fork-repo            "repos/fork/%s/%s"                  [username repo-name])
-(POST  create-repo          "repos/create"                      [repo-map])
-(POST  delete-repo          "repos/delete/%s"                   [repo-name])
-(POST  set-private-repo     "repos/set/private/%s"              [repo-map])
-(POST  set-public-repo      "repos/set/public/%s"               [repo-name])
-(GET   list-deploy-keys     "repos/keys/%s"                     [repo-name])
-(POST  add-deploy-key       "repos/key/%s/add"                  [key-map])
-(POST  remove-deploy-key    "repos/key/%s/add"                  [key-map])
-(GET   list-collaborators   "repos/show/%s/%s/collaborators"    [username repo-name])
-(POST  add-collaborator     "repos/collaborators/%s/add/%s"     [repo-name username])
-(POST  remove-collaborator  "repos/collaborators/%s/remove/%s"  [repo-name username])
-(GET   pushable-repos       "repos/pushable"                    [])
-(GET   repos-network        "repos/show/%s/%s/network"          [repo-name username])
-(GET   repos-languages      "repos/show/%s/%s/languages"        [repo-name username])
-(GET   repos-tags           "repos/show/%s/%s/tags"             [repo-name username])
-(GET   repos-branches       "repos/show/%s/%s/branches"         [repo-name username])
+(GET   search-repos         "repos/search/:q")
+(GET   get-repo-info        "repos/show/:user/:repo")
+(POST  update-repo-info     "repos/show/:user/:repo" (nested :description
+                                                             :homepage
+                                                             :has_wiki
+                                                             :has_issues
+                                                             :has_downloads))
+(GET   list-all-repos       "repos/show/:user")
+(POST  watch-repo           "repos/watch/:user/:repo")
+(POST  unwatch-repo         "repos/unwatch/:user/:repo")
+(POST  fork-repo            "repos/fork/:user/:repo")
+(POST  create-repo          "repos/create" (flat :name
+                                                 :description
+                                                 :homepage
+                                                 :public))
+(POST  delete-repo          "repos/delete/:repo")
+(POST  set-private-repo     "repos/set/private/:repo")
+(POST  set-public-repo      "repos/set/public/:repo")
+(GET   list-deploy-keys     "repos/keys/:repo")
+(POST  add-deploy-key       "repos/key/:repo/add" (flat :title :key))
+(POST  remove-deploy-key    "repos/key/:repo/remove" (flat :id))
+(GET   list-collaborators   "repos/show/:user/:repo/collaborators")
+(POST  add-collaborator     "repos/collaborators/:repo/add/:user")
+(POST  remove-collaborator  "repos/collaborators/:repo/remove/:user")
+(GET   pushable-repos       "repos/pushable")
+(GET   repos-network        "repos/show/:user/:repo/network")
+(GET   repos-languages      "repos/show/:user/:repo/languages")
+(GET   repos-tags           "repos/show/:user/:repo/tags")
+(GET   repos-branches       "repos/show/:user/:repo/branches")
 
 (defn remove-collab-from-all-projects [collab-user repo-owner]
-  (doseq [repo-name (map (key-fn "name") (list-all-repos repo-owner))]
+  (doseq [repo-name (map :name (list-all-repos repo-owner))]
     (remove-collaborator repo-name collab-user)
     (println "removed " collab-user " from " repo-name)))
