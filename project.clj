5e040b92663cb6529ca52c8b6b905b645b4f8b64
wilkes <wilkesjoiner@gmail.com>
2010-04-20 10:40:28 -0500

    added lein-clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject github-clj "0.1.0"
+(defproject github-clj "0.1.1"
   :description "A clojure wrapper around the Github API"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [clojure-http-client "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.1.0"]])
+  :dev-dependencies [[swank-clojure "1.1.0"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]])
