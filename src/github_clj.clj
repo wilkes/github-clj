fb4fcf1ba828381add2d8323fd26647fad61af6f
wilkes <wilkesjoiner@gmail.com>
2010-04-19 22:30:29 -0500

    clean up

--- src/github_clj.clj
+++ src/github_clj.clj
@@ -9,35 +9,24 @@
 (defvar *login*)
 (defvar *token*)
 
-(defmacro as-user [login token & body]
-  `(binding [*login* ~login *token* ~token]
-     ~@body))
-
 (defn auth-info []  {"login" *login* "token" *token*})
 
-(defn key-fn [s]
-  #(get % s))
-
-(defn apply-to-keys [f m]
-  (let [map-over-list (fn [l] (map (fn [x]
-                                     (if (map? x)
-                                       (apply-to-keys f x)
-                                       x))
-                                   l))
-        g (fn [x]
-            (cond
-             (map? x) (apply-to-keys f x)
-             (or (list? x) (vector? x)) (map-over-list x)
-             :default x))]
-    (reduce (fn [r [k v]] (conj r {(f k) (g v)}))
-            {}
-            m)))
+(defn map-keys [f m]
+  (let [listish? #(or (list? %) (vector %))
+        val-fn (fn [v]
+                 (cond
+                  (map? v) (map-keys f v)
+                  (listish? v) (map #(if (map? %) (map-keys f %) %) v)
+                  :default v))
+        reduce-fn  (fn [r [k v]]
+                     (conj r {(f k) (val-fn v)}))]
+    (reduce reduce-fn {} m)))
 
 (defn stringify-keys [m]
-  (apply-to-keys (fn [k] (if (keyword? k) (name k) (str k))) m))
+  (map-keys #(if (keyword? %) (name %) (str %)) m))
 
 (defn keywordify-keys [m]
-  (apply-to-keys keyword m))
+  (map-keys keyword m))
 
 (defn sub-action [action]
   (re-sub #"\*login\*" *login* action))
@@ -46,28 +35,30 @@
   [(re-gsub #":[a-z]+" "%s" s)
    (vec (map (comp symbol second) (re-seq #":([a-z]+)" s)))])
 
+(defn parse-json [body]
+  (try
+   (keywordify-keys (read-json-string body))
+   (catch Exception e
+     (throw (Exception. (str "Unable to parse:\n" body "\n\n")
+                        e)))))
+
 (defn parse-response
   ([response]
-     (parse-response response (comp keywordify-keys read-json-string)))
+     (parse-response response parse-json))
   ([response body-reader]
      (let [body (apply str (interpose "\n" (:body-seq response)))
            code (:code response)]
        (if (= 200 code)
-         (try
-          (body-reader body)
-          (catch Exception e
-            (throw (Exception. (str "Unable to parse:\n" body "\n\n")
-                               e))))
+         (body-reader body)
          (throw (Exception. (str code " " (:msg response))))))))
 
 (defn make-key-mapper [f keys]
   (fn [m]
-    (reduce (fn [result k]
-              (conj result (if-let [v (get m k)]
-                             {(f k) v}
-                             nil)))
-            {}
-            keys)))
+    (let [reduce-fn (fn [result k]
+                    (conj result (if-let [v (get m k)]
+                                   {(f k) v}
+                                   nil)))]
+      (reduce reduce-fn {} keys))))
 
 (defn nested [& keys]
   (make-key-mapper #(str "values[" (name %) "]") keys))
@@ -79,13 +70,11 @@
   (parse-response (request (str base-url action "?" (url-encode (auth-info))))))
 
 (defn do-post [action & [params]]
-  (parse-response (request (str base-url action)
-                           "POST"
-                           nil
-                           nil
-                           (if (map? params)
-                             (stringify-keys (merge params (auth-info)))
-                             (str params "&" (url-encode (auth-info)))))))
+  (let [post-data (if (map? params)
+                    (stringify-keys (merge params (auth-info)))
+                    (str params "&" (url-encode (auth-info))))
+        uri (str base-url action)]
+    (parse-response (request uri "POST" nil nil post-data))))
 
 (defmacro GET [name url-pattern]
   (let [[action-base action-params] (parse-url-str url-pattern)]
@@ -100,6 +89,10 @@
        (do-post (apply format (sub-action ~action-base)
                        (doall (map url-encode ~params)))
                 (~keys-fn form-params#)))))
+
+(defmacro as-user [login token & body]
+  `(binding [*login* ~login *token* ~token]
+     ~@body))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; User API
@@ -153,60 +146,56 @@
 (GET   repos-tags           "repos/show/:user/:repo/tags")
 (GET   repos-branches       "repos/show/:user/:repo/branches")
 
-(defn remove-collab-from-all-projects [collab-user repo-owner]
-  (doseq [repo-name (map :name (list-all-repos repo-owner))]
-    (remove-collaborator repo-name collab-user)
-    (println "removed " collab-user " from " repo-name)))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Commits API
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(GET list-commits "commits/list/:user_id/:repository/:branch")
-(GET list-file-commits "commits/list/:user_id/:repository/:branch/:path")
-(GET commit-info "commits/show/:user_id/:repository/:sha")
+(GET  list-commits       "commits/list/:user_id/:repository/:branch")
+(GET  list-file-commits  "commits/list/:user_id/:repository/:branch/:path")
+(GET  commit-info        "commits/show/:user_id/:repository/:sha")
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Issues API
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(GET  search-issues "issues/search/:user/:repo/:state/:search_term")
-(GET  list-project-issues "issues/list/:user/:repo/:state")
-(GET  show-issue "issues/show/:user/:repo/:number")
-(GET  list-issue-comments "issues/comments/:user/:repo/:number")
-(POST open-issue "issues/open/:user/:repo" (flat :title :body))
-(POST close-issue "issues/close/:user/:repo/:number")
-(POST reopen-issue "issues/reopen/:user/:repo/:number")
-(POST edit-issue "issues/edit/:user/:repo/:number" (flat :title :body))
-(GET  list-labels "issues/labels/:user/:repo")
-(POST add-label "issues/label/add/:user/:repo/:label/:number")
-(POST remove-label "issues/label/add/:user/:repo/:label/:number")
-(POST comment-on-issue "issues/comment/:user/:repo/:id" (flat :comment))
+(GET  search-issues        "issues/search/:user/:repo/:state/:search_term")
+(GET  list-project-issues  "issues/list/:user/:repo/:state")
+(GET  show-issue           "issues/show/:user/:repo/:number")
+(GET  list-issue-comments  "issues/comments/:user/:repo/:number")
+(POST open-issue           "issues/open/:user/:repo" (flat :title :body))
+(POST close-issue          "issues/close/:user/:repo/:number")
+(POST reopen-issue         "issues/reopen/:user/:repo/:number")
+(POST edit-issue           "issues/edit/:user/:repo/:number" (flat :title :body))
+(GET  list-labels          "issues/labels/:user/:repo")
+(POST add-label            "issues/label/add/:user/:repo/:label/:number")
+(POST remove-label         "issues/label/add/:user/:repo/:label/:number")
+(POST comment-on-issue     "issues/comment/:user/:repo/:id" (flat :comment))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Network API
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn network-api-call [url-fmt params user repo]
+  (parse-response (request (str (format url-fmt user repo) "?" params))))
+
 (defn network-meta [user repo]
-  (parse-response
-   (request (str (format "https://github.com/%s/%s/network_meta" user repo)
-                 "?" (url-encode (auth-info))))))
+  (network-api-call "https://github.com/%s/%s/network_meta"
+                    (url-encode (auth-info))
+                    user repo))
 
 (defn network-data-chunk [user repo nethash]
-  (parse-response
-   (request (str (format "https://github.com/%s/%s/network_data_chunk?" user repo)
-                 "?" (url-encode (merge {"nethash" nethash} (auth-info)))))))
+  (network-api-call "https://github.com/%s/%s/network_data_chunk?"
+                    (url-encode (merge {"nethash" nethash} (auth-info)))
+                    user repo))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Object API
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(GET tree-contents "tree/show/:user/:repo/:tree_sha")
-(GET blob-contents-by-tree "blob/show/:user/:repo/:tree_sha/:path")
-(GET list-blobs "blob/all/:user/:repo/:tree_sha")
+(GET tree-contents          "tree/show/:user/:repo/:tree_sha")
+(GET blob-contents-by-tree  "blob/show/:user/:repo/:tree_sha/:path")
+(GET list-blobs             "blob/all/:user/:repo/:tree_sha")
 
 (defn blob-contents [user repo sha]
-  (parse-response
-   (request (str base-url
+  (let [url (str base-url
                  (format "blob/show/%s/%s/%s" user repo sha)
-                 "?" (url-encode (auth-info))))
-   identity))
-
+                 "?" (url-encode (auth-info)))]
+    (parse-response (request url) identity)))
