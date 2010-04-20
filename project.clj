eda56aecccb4ce4dbc3bfaf0047290e0f83e090f
wilkes <wilkesjoiner@gmail.com>
2010-04-20 10:21:09 -0500

    added MIT License and copyright

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject github-clj "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject github-clj "0.1.0"
+  :description "A clojure wrapper around the Github API"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [clojure-http-client "1.0.0-SNAPSHOT"]]
